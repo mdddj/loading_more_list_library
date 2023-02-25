21d23f3123205b29581bf5df4548b50285052b79
梁典典 <413153189@qq.com>
2023-02-25 14:14:34 +0800

    :rocket: 更换为性能更高的fast_immutable_collections

--- lib/src/loading_more_list_library.dart
+++ lib/src/loading_more_list_library.dart
@@ -6,23 +6,25 @@ import 'package:meta/meta.dart';
 
 abstract class RefreshBase {
   Future<bool> refresh([bool notifyStateChanged = false]);
+
   Future<bool> errorRefresh();
 }
 
-enum IndicatorStatus {
-  none,
-  loadingMoreBusying,
-  fullScreenBusying,
-  error,
-  fullScreenError,
-  noMoreLoad,
-  empty
+extension IListEx<T> on IList<T> {
+  IList<T> updateItemEx(T oldItem, T Function(T old) call) {
+    final int index = indexOf(oldItem);
+    final T newItem = call(get(index));
+    return replace(index, newItem);
+  }
 }
 
-abstract class LoadingMoreBase<T> extends ListBase<T>
-    with _LoadingMoreBloc<T>, RefreshBase {
+enum IndicatorStatus { none, loadingMoreBusying, fullScreenBusying, error, fullScreenError, noMoreLoad, empty }
+
+abstract class LoadingMoreBase<T> extends ListBase<T> with _LoadingMoreBloc<T>, RefreshBase {
   IList<T> _array = <T>[].lock;
+
   IList<T> get array => _array;
+
   set array(IList<T> data) => _array = data;
 
   @override
@@ -77,6 +79,7 @@ abstract class LoadingMoreBase<T> extends ListBase<T>
   Future<bool> refresh([bool notifyStateChanged = false]) async {
     if (notifyStateChanged) {
       clear();
+      isLoading = false;
       indicatorStatus = IndicatorStatus.fullScreenBusying;
       onStateChanged(this);
     }
@@ -94,6 +97,7 @@ abstract class LoadingMoreBase<T> extends ListBase<T>
 
   @override
   int get length => _array.length;
+
   @override
   set length(int newLength) => _array.unlock.length = newLength;
 
@@ -105,8 +109,7 @@ abstract class LoadingMoreBase<T> extends ListBase<T>
   }
 
   bool get hasError {
-    return indicatorStatus == IndicatorStatus.fullScreenError ||
-        indicatorStatus == IndicatorStatus.error;
+    return indicatorStatus == IndicatorStatus.fullScreenError || indicatorStatus == IndicatorStatus.error;
   }
 
   /// update ui
@@ -114,12 +117,30 @@ abstract class LoadingMoreBase<T> extends ListBase<T>
     super.onStateChanged(this);
   }
 
+  T getWithIndex(int index, {T Function(int index)? orElse}) {
+    if (orElse == null) {
+      return this[index];
+    }
+    return (index < 0 || index >= length) //
+        ? orElse(index)
+        : this[index];
+  }
 
+  T updateItem(T oldItem, T Function(T old) call, [bool updateUi = false]) {
+    final int index = _array.indexOf(oldItem);
+    final T newItem = call(get(index));
+    _array = _array.replace(index, newItem);
+    if (updateUi) {
+      setState();
+    }
+    return newItem;
+  }
 
   @override
   void add(T element) {
-   _array = _array.add(element);
+    _array = _array.add(element);
   }
+
   @override
   void addAll(Iterable<T> iterable) {
     _array = _array.addAll(iterable);
@@ -128,13 +149,22 @@ abstract class LoadingMoreBase<T> extends ListBase<T>
   @override
   void clear() {
     _array = _array.clear();
-    super.clear();
+  }
+
+  @override
+  void removeWhere(bool Function(T element) test) {
+    _array = _array.removeWhere(test);
+  }
+
+  @override
+  void insert(int index, T element) {
+    _array = _array.insert(index, element);
   }
 }
 
 class _LoadingMoreBloc<T> {
-  final StreamController<LoadingMoreBase<T>> _rebuild =
-      StreamController<LoadingMoreBase<T>>.broadcast();
+  final StreamController<LoadingMoreBase<T>> _rebuild = StreamController<LoadingMoreBase<T>>.broadcast();
+
   Stream<LoadingMoreBase<T>> get rebuild => _rebuild.stream;
 
   void onStateChanged(LoadingMoreBase<T> source) {
